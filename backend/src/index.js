ac20cf9c3620477329da5c57f4f113a5a3798824
Juan Pablo Ballistreri <jpballistreri@gmail.com>
2022-02-13 04:10:50 -0300

    fix backend body-parser

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,9 +25,9 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Authorization, X-API-Key, Origin, X-Requested-With, Content-type, Accept, Access-Control-Allow-Request-Method"
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
   );
-  res.header("Access-ControlAllow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
